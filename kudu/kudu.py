441694b1d11e39f517195961d490a73cf7a6867d
Humphrey Huang <18270821+humphreyh@users.noreply.github.com>
2018-07-13 12:44:35 -0500

    add readme and update script

--- kudu/kudu.py
+++ kudu/kudu.py
@@ -1,19 +1,32 @@
-import urllib2
+import requests
 import json
 import argparse
+import sys
 
 
 def main(servers):
     metrics = []
     for i in range(len(servers)):
-        server = servers[i]
-        url = "{}/metrics".format(server)
-        response = urllib2.urlopen(url)
-        data = json.loads(response.read())
-        metrics_to_append = parse_metrics(data)
-        for m in metrics_to_append:
-            metrics.append(m)
+        try:
+            server = servers[i]
+            url = "{}/metrics".format(server)
+            response = requests.get(url)
+            if not response.raise_for_status():
+                data = response.json()
+                metrics_to_append = parse_metrics(data)
+                metrics.extend(metrics_to_append)
+        except requests.exceptions.InvalidSchema as e:
+            print("Verify server URL. Ensure http or https is specified.")
+            sys.exit(1)
+        except requests.exceptions.ConnectionError as e:
+            print("Cannot connect to this server URL: '{}'".format(server))
+            sys.exit(1)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
 
+    if not metrics:
+        raise Exception("No metrics available")
     print(json.dumps(metrics, ensure_ascii=False))
 
 
@@ -23,39 +36,54 @@ def parse_metrics(all_metrics):
     for item in all_metrics:
         metrics_list = item.get("metrics")
 
-        if item.get("type") == "server":
-            metrics = {}
-            prefix = item.get("id")
-            prefix = prefix[5:]
-            for m in metrics_list:
-                name = "{}.{}".format(prefix, m.get("name").
-                                      replace("_kudu", ""))
-                val = m.get("value")
-                if val is not None:
-                    metrics[name] = val
-                else:
-                    for key in m:
-                        if key != "name":
-                            metric_name = "{}.{}".format(name, key)
-                            metrics[metric_name] = m[key]
-            metrics_to_return.append(metrics)
+        if metrics_list:
+            # obtain server (tablet or master) metrics
+            if item.get("type") == "server":
+                metrics = {}
+                # id specifies whether it is a tablet server or master
+                # eg. kudu.tabletserver or kudu.master
+                prefix = item.get("id")
+                # skip if server type is unknown
+                if prefix:
+                    # Telegraf configuration will add 'kudu' as a prefix
+                    prefix = prefix.replace("kudu.", "")
+                    for m in metrics_list:
+                        # some server metric names contain another 'kudu'
+                        name = "{}.{}".format(prefix, m.get("name").
+                                              replace("_kudu", ""))
+                        val = m.get("value")
+                        if val is not None:
+                            metrics[name] = val
+                        # histogram metrics do not have a 'value' key
+                        else:
+                            for key in m:
+                                if key != "name":
+                                    metric_name = "{}.{}".format(name, key)
+                                    metrics[metric_name] = m[key]
+                    metrics_to_return.append(metrics)
 
-        if item.get("type") == "tablet":
-            tablets = {}
-            tag = item.get("attributes").get("table_name")
-            tablets["tablet"] = tag
-            prefix = "master.tablets"
-            for m in metrics_list:
-                name = "{}.{}".format(prefix, m.get("name"))
-                val = m.get("value")
-                if val is not None:
-                    tablets[name] = val
-                else:
-                    for key in m:
-                        if key != "name":
-                            metric_name = "{}.{}".format(name, key)
-                            tablets[metric_name] = m[key]
-            metrics_to_return.append(tablets)
+            # obtain tablet metrics
+            if item.get("type") == "tablet":
+                tablets = {}
+                # set table name as a point tag for metrics
+                tag = item.get("attributes").get("table_name")
+                # skip if table is unknown
+                if tag is not None:
+                    tablets["table"] = tag
+                    # id is not as useful so setting our own prefix
+                    prefix = "master.tablets"
+                    for m in metrics_list:
+                        name = "{}.{}".format(prefix, m.get("name"))
+                        val = m.get("value")
+                        if val is not None:
+                            tablets[name] = val
+                        # histogram metrics do not have a 'value' key
+                        else:
+                            for key in m:
+                                if key != "name":
+                                    metric_name = "{}.{}".format(name, key)
+                                    tablets[metric_name] = m[key]
+                    metrics_to_return.append(tablets)
 
     return metrics_to_return
 
