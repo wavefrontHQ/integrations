d0cf5059d3d60b9fd447c42b6eed2364942d52c3
yogeshprasad <yogeshprasadkurmi@gmail.com>
2019-07-17 12:49:38 +0530

    Adding Dynatrace Collector

---
+++ dynatrace_collector/README.md
@@ -0,0 +1,55 @@
+## Overview
+The Wavefront Dynatrace collector collects metrics from Dynatrace and pushes to the wavefront.
+
+## Installation
+
+Install using the provided setup.py
+- Clone this repository.
+- Change directory to dynatrace_collector
+- Run below command to install Dynatrace collector
+
+```
+pip3 install .
+```
+
+## Configure Dynatrace Collector
+Run Below command to configure Dynatrace Collector
+```
+wf-dynatrace config -b <dynatrace-base-url> -a <dynatrace-api-key> -s <wavefront-proxy-ip> 
+```
+
+This command will store the configuration data in a secure file and later that data will be used for collecting the data from Dynatrace and sending data to the Wavefront.
+
+## Start Dynatrace Collector
+- Running collector in foreground
+```
+wf-dynatrace run
+```
+
+- Running collector as a daemon
+```
+wf-dynatrace run -d true
+```
+
+### Dynatrace Collector as Service
+- Start collector
+```
+dynatrace-collector start
+```
+
+- Stop collector
+```
+dynatrace-collector stop
+```
+- Check Service status
+```
+dynatrace-collector status
+```
+
+- Restart  collector
+```
+dynatrace-collector restart
+```
+
+### Metrics Configuration
+The metrics configuration file will be located in the `/etc/wavefront/dynatrace/config/config.json`, use this configuration file to enable/disable the metrics collection.
---
+++ dynatrace_collector/setup.py
@@ -0,0 +1,70 @@
+"""
+Setup script for the Wavefront collector tools.
+"""
+
+import os
+import setuptools
+import setuptools.command.install
+from shutil import copyfile
+from setuptools.command.install import install
+
+
+# The easiest way to convert the markdown to RestructuredText is to use
+# pandoc.  There is a Python frontend to that package called pypandoc.
+# To use this code you will need to :
+#   1. Download and install pandoc  (http://pandoc.org/installing.html)
+#   2. pip install pypandoc
+# see: https://coderwall.com/p/qawuyq/use-markdown-readme-s-in-python-modules
+try:
+    import pypandoc
+    LONG_DESCRIPTION = pypandoc.convert_file(source_file='README.md',
+                                             format='markdown_github',
+                                             to='rst',
+                                             extra_args=['-s', '--columns=1000'])
+except (IOError, ImportError):
+    LONG_DESCRIPTION = ''
+
+class PostInstallCommand(install):
+
+  def run(self):
+
+    base_dir = '/Users/ykurmi/etc/wavefront/dynatrace/'
+    config_dir = base_dir + 'config/'
+    log_dir = base_dir + 'log/'
+    pid_dir = base_dir + 'pid/'
+
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+
+    copyfile('config/config.json', config_dir+'config.json')
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    open(log_dir+'dynatrace.log', 'w+')
+
+    if not os.path.exists(pid_dir):
+        os.makedirs(pid_dir)
+
+    install.run(self)
+
+setuptools.setup(
+    name='dynatrace_collector',
+    version='0.0.1',
+    author='Wavefront',
+    author_email='mike@wavefront.com',
+    description=('Dynatrace Collector'),
+    license='BSD',
+    long_description=LONG_DESCRIPTION,
+    keywords='wavefront wavefront_integration collector metrics',
+    url='https://www.wavefront.com',
+    install_requires=['wavefront-sdk-python', 'python-daemon>=2.1.1'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+    ],
+    package_data={'wavefront': ['config/*']},
+    cmdclass={'install': PostInstallCommand},
+    scripts=['wf-dynatrace', 'dynatrace-collector']
+)
